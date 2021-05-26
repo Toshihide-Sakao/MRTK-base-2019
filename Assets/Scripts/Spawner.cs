0f2922f8fdfeb0c00c8216005f48a7c8e37676bd
Toshihide-Sakao <70141444+Toshihide-Sakao@users.noreply.github.com>
2021-05-26 10:41:16 +0200

    Ok

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+using System.Threading.Tasks.Dataflow;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    float spawnDistance = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
